0c00ff11350536c4b8f1f8fd21050627e12684f1
rusafe <150953396+rusafe@users.noreply.github.com>
2025-01-07 19:14:02 +0100

    Started model and added cards to local directory

--- src/main/Model.java
+++ src/main/Model.java
@@ -1,5 +1,31 @@
 package main;
 
-public class Model {
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Base64;
 
+public class Model {
+	private MongoClient dbClient;
+	private MongoDatabase db;
+	private final File cardsDirectory = new File("resources/cards");
+	
+	public void openConnection() {
+		
+	}
+	
+	public void closeConnection() {
+		
+	}
+	
+	public void loadCardsToDb() {
+		
+	}
+	
+	private String encodeImageToBase64(File image) throws Exception {
+		String encodedImage;
+		FileInputStream fis = new FileInputStream(image);
+		encodedImage = new String(Base64.getEncoder().encode(fis.readAllBytes()));
+		fis.close();
+		return encodedImage;
+	}
 }
