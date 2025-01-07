276cb2e292d3d0851373683462282073987634a2
Aumabe <aumabe@alumnatflorida.es>
2025-01-07 19:13:16 +0100

    Made the project a Maven project

--- src/main/Model.java
+++ src/main/Model.java
@@ -1,5 +1,23 @@
 package main;
 
-public class Model {
+import org.bson.Document;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class Model {
+	private String sessionUsername;
+	private String sessionPwd;
+	private MongoClient mongoClient = new MongoClient("localhost", 27017);
+	private MongoDatabase database = mongoClient.getDatabase("Casino");
+	
+	public boolean signUpUser(String username, String pwd) {
+		MongoCollection<Document> coleccion = database.getCollection("users");
+		Document newUser = new Document("user", username).append("pass", pwd);
+	}
+	
+	private boolean userExists(String username, String pwd) {
+		
+	}
 }
